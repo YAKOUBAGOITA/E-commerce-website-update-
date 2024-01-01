6039403e2bb1171c5252525f54c7a6a7a173c94e
yakouba <yakoubagoita393@gmail.com>
2024-01-02 01:13:15 +0530

    Fetching and displaying products, product_title, product_description...

--- index.php
+++ index.php
@@ -1,253 +1,124 @@
 <!-- connect file-->
 <?php
- include('includes/connect.php');
+include('includes/connect.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-       <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title>E-Commerce Website</title>
 
-       <!-- bootstrap  css link-->
-       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-       <!-- font awesome link-->
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
-      <!-- css link -->
-      <link rel="stylesheet" href="style.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce Website</title>
+
+    <!-- bootstrap css link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- font awesome link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- css link -->
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-      <!-- navbar -->
-      <div class="container-fluid p-0">
-        <!-- first child-->
-        <nav class="navbar navbar-expand-lg bg-info">
-  <div class="container-fluid">
-    <img src="./images/logo.jpg" alt="" class="logo">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-     aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-0 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Products</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Register</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contacts</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i>
-
-          </i><sup>1</sup></a>
-        </li>  
-        <li class="nav-item">
-          <a class="nav-link" href="#">Total price:100/-</a>
-        </li>
-      </ul>
-      <form class="d-flex p-0">
-        <input class="form-control me-0 p-0" type="search" placeholder="Search"
-         aria-label="Search">
-        <button class="btn btn-outline-light" type="submit">Search</button>
-      </form>
+    <!-- navbar -->
+    <div class="container-fluid p-0">
+        <!-- ... (your existing code) ... -->
     </div>
-  </div>
-</nav>
 
-<!-- second child -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
-       <ul class="navbar-nav me-auto">
-           <li class="nav-item">   
-              <a class="nav-link" href="#">Welcome Guest</a>    
-            </li>
-            <li class="nav-item">   
-              <a class="nav-link" href="#">Login</a>   
-            </li>
-       </ul>
-</nav>
+    <!-- fourth child -->
+    <div class="row">
+        <div class="col-md-10">
+            <!--Products-->
+            <div class="row px-3">
+                <!-- fetching products -->
+                <?php
+                $select_query = "SELECT * FROM `products`order by rand()";
+                $result_query = mysqli_query($con, $select_query);
 
-<!-- third child -->
-<div class="bg-light">
-       <h3 class="text-center">Hidden Store</h3>
-       <p class="text-center">Communications is at the heart of 
-        e-commerce  and community</p>
-</div>
+                while ($row = mysqli_fetch_assoc($result_query)) {
+                    $product_id = mysqli_real_escape_string($con, $row['product_id']);
+                    $product_title = mysqli_real_escape_string($con, $row['product_title']);
+                    $product_description = mysqli_real_escape_string($con, $row['product_description']);
+                    $product_image1 = $row['product_image1'];
+                    $product_price = $row['product_price'];
+                    $category_id = $row['category_id'];
+                    $brand_id = $row['brand_id'];
+                    ?>
 
-<!-- fourth child -->
-<div class="row">
-
-<!--Products-->
-    <div class="col-md-10">
-      <div class="row">
-       <div class="col-md-4 mb-2">
-          <div class="card">
-           <img src="./images/juice.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example
-              text to build on the card title and make 
-              up the bulk of the card's content.</p>
-              <div class="d-flex p-0">
-              <a href="#" class="btn bg-info">Add to cart</a>
-              <a href="#" class="btn bg-secondary">View more</a>
-              </div>
+                    <div class="col-md-4 mb-2">
+                        <div class="card">
+                            <img src="./admin_area/product_images/<?php echo $product_image1; ?>" 
+                            class="card-img-top" alt="product_title">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $product_title; ?></h5>
+                                <p class="card-text"><?php echo $product_description; ?></p>
+                                <div class="d-flex p-0">
+                                    <a href="#" class="btn bg-info">Add to cart</a>
+                                    <a href="#" class="btn bg-secondary">View more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
-         </div>
-       </div>
+        </div>
 
-       <div class="col-md-4 mb-2">
-         <div class="card">
-           <img src="./images/jeans.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-             <h5 class="card-title">Card title</h5>
-             <p class="card-text">Some quick example text to build 
-             on the card title and make up the bulk of the card's content.</p>
-             <div class="d-flex p-0">
-             <a href="#" class="btn bg-info">Add to cart</a>
-             <a href="#" class="btn bg-secondary">View more</a>
-             </div>
-            </div>
-         </div>
-       </div>
+        <!-- sidenav -->
+        <div class="col-md-2 bg-secondary p-0">
+            <ul class="navbar-nav me-auto">
+                <!-- brands to be displayed -->
+                <li class="nav-item bg-info">
+                    <a href="" class="nav-link">Delivery Brands</a>
+                </li>
 
-       <div class="col-md-4 mb-2">
-         <div class="card">
-          <img src="./images/shoes.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-             <h5 class="card-title">Card title</h5>
-             <p class="card-text">Some quick example text to build on the card 
-              title and make up the bulk of the card's content.</p>
-              <div class="d-flex p-0">
-              <a href="#" class="btn bg-info">Add to cart</a>
-              <a href="#" class="btn bg-secondary">View more</a>
-              </div>
-           </div>
-          </div>
-       </div>
+                <?php
+                $select_brands = "SELECT * FROM `brands`";
+                $result_brands = mysqli_query($con, $select_brands);
 
-       <div class="col-md-4 mb-2">
-           <div class="card">
-            <img src="./images/shoe.jpg" class="card-img-top" alt="...">
-             <div class="card-body">
-               <h5 class="card-title">Card title</h5>
-               <p class="card-text">Some quick example text to build on the card 
-                title and make up the bulk of the card's content.</p>
-                <div class="d-flex p-0">
-                <a href="#" class="btn bg-info">Add to cart</a>
-                <a href="#" class="btn bg-secondary">View more</a>
-               </div>
-             </div>
-           </div>
-       </div>
+                while ($row_data = mysqli_fetch_assoc($result_brands)) {
+                    $brand_title = $row_data['brand_title'];
+                    $brand_id = $row_data['brand_id'];
+                    echo "<li class='nav-item'>
+                            <a href='index.php?brand=$brand_id' class='nav-link text-light text-center'>$brand_title</a>
+                          </li>";
+                }
+                ?>
 
-       <div class="col-md-4 mb-2">
-         <div class="card">
-           <img src="./images/mango.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card
-                title and make up the bulk of the card's content.</p>
-                <div class="d-flex p-0">
-                <a href="#" class="btn bg-info">Add to cart</a>
-                <a href="#" class="btn bg-secondary">View more</a>
-                </div>
-             </div>
-         </div>
-       </div>
+                <!-- categories to be displayed -->
+                <li class="nav-item bg-info">
+                    <a href="" class="nav-link text-center">Categories</a>
+                </li>
+                <?php
+                $select_categories = "SELECT * FROM `categories`";
+                $result_categories = mysqli_query($con, $select_categories);
 
-       <div class="col-md-4 mb-2">
-         <div class="card">
-           <img src="./images/apple.jpg" class="card-img-top" alt="...">
-             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card
-                title and make up the bulk of the card's content.</p>
-                 <div class="d-flex p-0">
-                 <a href="#" class="btn bg-info">Add to cart</a>
-                  <a href="#" class="btn bg-secondary">View more</a>
-               </div>
-             </div>
-         </div>
-       </div>
- 
-        
-      </div>
-    </div> 
+                while ($row_data = mysqli_fetch_assoc($result_categories)) {
+                    $category_title = $row_data['category_title'];
+                    $category_id = $row_data['category_id'];
+                    echo "<li class='nav-item'>
+                            <a href='index.php?category=$category_id' class='nav-link text-light text-center'>$category_title</a>
+                          </li>";
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
 
+    <!-- last child -->
+    <div class="bg-info p-3 text-center">
+        <p>All rights © reserved Designed by Yakouba_Goita-2023</p>
+    </div>
 
-
-<!-- sidenav -->   
- <div class="col-md-2 bg-secondary p-0">
-  <ul class="navbar-nav me-auto">
-       <!-- brands to be displayed -->
-     <li class="nav-item bg-info">
-       <a href="" class="nav-link">Delivery Brands</a>
-     </li> 
-
-     <?php 
-     $select_brands="select* from `brands`";
-     $result_brands=mysqli_query($con,$select_brands);
-     //$row_data=mysqli_fetch_assoc($result_brands);
-    // echo $row_data['brand_title'];
-
-    while($row_data=mysqli_fetch_assoc($result_brands)){
-       $brand_title=$row_data['brand_title'];
-       $brand_id=$row_data['brand_id'];
-       echo  "<li class='nav-item'> 
-       <a href='index.php?brand=$brand_id' 
-       class='nav-link text-light text-center'> $brand_title</a>
-       </li>";
-    }
-       ?>
-    
-     <!-- categories  to be displayed -->
-     <li class="nav-item bg-info">
-        <a href="" class="nav-link text-center">Categories</a>
-     </li> 
-     <?php 
-        $select_categories="select* from `categories`";
-        $result_categories=mysqli_query($con,$select_categories);
-        //$row_data=mysqli_fetch_assoc($result_brands);
-       // echo $row_data['brand_title'];
-       while($row_data=mysqli_fetch_assoc($result_categories)){
-       $category_title=$row_data['category_title'];
-       $category_id=$row_data['category_id'];
-        echo  "<li class='nav-item'> 
-       <a href='index.php?category=$category_id' 
-       class='nav-link text-light text-center'> $category_title</a>
-       </li>";
-        }
-     ?> 
-
-  </ul>    
- </div>    
-</div>
-
-
-
-<!-- last child -->
-
-<div class="bg-info p-3 text-center">
- <p>All rights © reserved  Designed by Yakouba_Goita-2023</p>
-</div>  
-
-</div>
-       
-         <!--bootstrap js link-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-crossorigin="anonymous"></script>
+    <!-- bootstrap js link-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
